82ba78f7a1ca606446bc96dcd38966d0c9ca4863
Estrella Benítez Buitrago <156102986+Llitabb@users.noreply.github.com>
2024-03-01 20:33:02 +0000

    nuevo commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//estos son las variables para los elementos que luego generará
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+//función condicional para generar todas las posibilidades de dominio que
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      let dominio = pronoun[i] + adj[j] + noun[k] + ".com";
+      {
+        console.log(dominio);
+      }
+    }
+  }
+}
+
+//window.onload = function() {
+//write your code here
+// console.log("Hello Rigo from the console!");};
